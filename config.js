8947f8307f27ff4fd9a84df319e14778687eb1b5
BakunawaWorld <98802863+BakunawaWorld@users.noreply.github.com>
2022-02-09 18:01:07 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGO_URI || 'mongodb://localhost/React-Redux-Node-Mongodb'
+  database: process.env.MONGO_URI || 'mongodb+srv://bakunawa:qaz1wsx2@cluster0.3ukm2.mongodb.net/React-Redux-Node-Mongodb'
 };
